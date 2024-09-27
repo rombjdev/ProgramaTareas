e9fc9b1a77d470a627d3248698979c377f8edb25
Roman <romanbarriosj.dev@gmail.com>
2024-09-27 13:42:35 -0300

    Añadimos el marcar tareas como completadas y eliminar tarea completada

--- main.cpp
+++ main.cpp
@@ -1,12 +1,21 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 int main(){
+    /*
+        Sacaremos lo siguiente:
+            std::string tareas[MAX_TAREAS];
+            const int MAX_TAREAS = 10;
+            int totalTareas = 0;        
+         para incorporar vectores, lo que nos permitirá trabajar con listas de tamaño dinámico
+    */
 
-    const int MAX_TAREAS = 10;
-    int totalTareas = 0;
+    std::vector<std::string> tareas;
+    std::vector<bool> estadoTareas;
+
     int opcion = 0;
-    std::string tareas[MAX_TAREAS];
+    
 
     do
     {
@@ -15,7 +24,8 @@ int main(){
         std::cout << "1. Añadir tarea" << std::endl;;
         std::cout << "2. Ver tareas" << std::endl;
         std::cout << "3. Marcar tarea como completada" << std::endl;
-        std::cout << "4. Salir" << std::endl;
+        std::cout << "4. Eliminar tarea completada" << std::endl;
+        std::cout << "5. Salir" << std::endl;
         std::cout << "Elige una opcion: "<< std::endl;;
         std::cin >> opcion;
         std::cin.ignore();  // Ignorar el salto de línea que deja std::cin
@@ -23,30 +33,30 @@ int main(){
         switch (opcion)
         {
             case 1:
-                if(totalTareas < MAX_TAREAS){
+                {
+                    std::string nuevaTarea;
                     std::cout << "" << std::endl;;
                     std::cout << "Introduce la nueva tarea: " << std::endl;
-                    std::getline(std::cin, tareas[totalTareas]);
-                    totalTareas++ ;
-                }else{
-                    std::cout << "" << std::endl;;
-                    std::cout << "Has alcanzado el máximo de tareas permitido."<< std::endl;
+                    std::getline(std::cin, nuevaTarea);
+                    tareas.push_back(nuevaTarea);
+                    estadoTareas.push_back(false);
+
                 }
                 break;
             case 2:
                 std::cout << "" << std::endl;
                 std::cout << "Lista de tareas:"<< std::endl;
-                if(totalTareas == 0){
+                if(tareas.empty()){
                     std::cout << "" << std::endl;
                     std::cout << "No hay tareas en la lista" << std::endl;
                 }else{
-                    for(int i = 0; i < totalTareas; i++){
-                        std::cout << "." << tareas[i] << std::endl;
+                    for(int i = 0; i < tareas.size(); i++){
+                        std::cout << "." << tareas[i] << (estadoTareas[i] ? " (Completada)" : " (Pendiente)") <<  std::endl;
                     }
                 }
                 break;
             case 3:
-                if(totalTareas == 0){
+                if(tareas.empty()){
                     std::cout << "" << std::endl;
                     std::cout << "No hay tareas, para añadir una seleccione la opción 1." << std::endl;        
                 }else{
@@ -55,14 +65,17 @@ int main(){
                     std::cout << "Introduce el numero de la tarea completada." << std::endl;
                     std::cin >> tareaCompletada;
 
-                    if(tareaCompletada >= 1 && tareaCompletada <=totalTareas){
+                    if(tareaCompletada >= 1 && tareaCompletada <= tareas.size()){
                         std::string tarea = tareas[tareaCompletada-1];
-                        for(int i = tareaCompletada-1; i < totalTareas-1; i++){
-                            tareas[i] = tareas[i + 1];
-                        }
-                        totalTareas--;
+                        estadoTareas[tareaCompletada-1] = true;
+
+                        //std::string tarea = tareas[tareaCompletada-1];
+                        // for(int i = tareaCompletada-1; i < tareas.size()-1; i++){
+                            
+                        //     tareas.erase(tareas.begin() + (tareaCompletada-1));
+                        // }
                         std::cout << "" << std::endl;
-                        std::cout << "Tarea '" << tarea << "' completada y eliminada" << std::endl;
+                        std::cout << "Tarea '" << tarea << "' marcada como completada" << std::endl;
                     }else {
                         std::cout << "" << std::endl;
                         std::cout << "Numero de la tarea es invalido." << std::endl;
@@ -71,6 +84,43 @@ int main(){
                 }
                 break;
             case 4:
+                if(tareas.empty()){
+                    std::cout << "" << std::endl;
+                    std::cout << "No hay tareas, para añadir una seleccione la opción 1." << std::endl;        
+                }else{
+                    int tareaCompletada;
+                    std::cout << "" << std::endl;
+                    std::cout << "Introduce el numero de la tarea que quieres eliminar." << std::endl;
+                    std::cin >> tareaCompletada;
+
+                    if(tareaCompletada >= 1 && tareaCompletada <= tareas.size()){
+                        std::string tarea = tareas[tareaCompletada-1];
+                        estadoTareas[tareaCompletada-1] = true;
+
+                        if(estadoTareas[tareaCompletada-1] == true){
+                            tareas.erase(tareas.begin() + (tareaCompletada-1));
+                            estadoTareas.erase(estadoTareas.begin() + (tareaCompletada-1));
+                        }else{
+                            std::cout << "" << std::endl;
+                            std::cout << "la tarea '" << tarea << "' no está marcada como completada" << std::endl;                        
+                        }
+
+                        //std::string tarea = tareas[tareaCompletada-1];
+                        // for(int i = tareaCompletada-1; i < tareas.size()-1; i++){
+                            
+                        //     tareas.erase(tareas.begin() + (tareaCompletada-1));
+                        // }
+                        std::cout << "" << std::endl;
+                        std::cout << "Tarea '" << tarea << "' marcada como completada" << std::endl;
+                    }else {
+                        std::cout << "" << std::endl;
+                        std::cout << "Numero de la tarea es invalido." << std::endl;
+                    }
+                       
+                }
+                break;
+
+            case 5:
                 std::cout << "" << std::endl;
                 std::cout << "Saliendo del programa..." << std::endl;
                 break;    
@@ -83,7 +133,7 @@ int main(){
 
 
 
-    } while (opcion != 4);
+    } while (opcion != 5);
     
 
 
